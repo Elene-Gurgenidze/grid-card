bc67663e8b42029857428e41005a68ac10233e5c
Elene-Gurgenidze <elenegurgenidze81@gmail.com>
2025-11-08 12:51:52 +0400

    add React project files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import GridCards from "./components/GridCards";
+
+function App() {
+  return (
+    <div>
+      <GridCards />
+    </div>
+  );
+}
+
+export default App;
